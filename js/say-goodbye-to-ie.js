2229d2dc426ae4cb79f4b2b2c17a4705dee5eee3
likaijunlkj <1956946076@QQ.com>
2020-04-09 08:23:10 +0800

    first commit

---
+++ js/say-goodbye-to-ie.js
@@ -0,0 +1,31 @@
+﻿var getExplorer = (function () {
+    var explorer = window.navigator.userAgent,
+        compare = function (s) { return (explorer.indexOf(s) >= 0); },
+        ie11 = (function () { return ("ActiveXObject" in window) })();
+    if (compare("MSIE") || ie11) { return 'ie'; }
+    else if (compare("Firefox") && !ie11) { return 'Firefox'; }
+    else if (compare("Chrome") && !ie11) {
+        if (explorer.indexOf("Edge") > -1) {
+            return 'Edge';
+        } else {
+            return 'Chrome';
+        }
+    }
+    else if (compare("Opera") && !ie11) { return 'Opera'; }
+    else if (compare("Safari") && !ie11) { return 'Safari'; }
+
+})()
+
+var version = navigator.userAgent;
+
+if (getExplorer == 'ie') {
+    window.location.href='https://lijiajunljj.github.io/ie';
+}
+
+if (getExplorer == 'Edge') {
+	if(version.indexOf("Windows NT 10")!=-1){
+		    console.log("检测到现在使用的是win10系统且正在使用Edge，不做跳转");
+}else {
+    window.location.href='https://lijiajunljj.github.io/ie';
+      }
+}
